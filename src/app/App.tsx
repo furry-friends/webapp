c7701d1162f03b05bc42d452805c99a1933ae35c
Zhiguang Chen <zhiguang.c@gmail.com>
2023-10-10 15:30:35 +0200

    Create Footer component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header/Header';
+import Footer from './Footer/Footer';
 
 const App = (): JSX.Element => {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 };
